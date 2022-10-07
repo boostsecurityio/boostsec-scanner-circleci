7d18fb24515c93091b53cfe40b3d49848e7574fd
Jonathan Serafini <jonathan@serafini.ca>
2022-10-07 09:38:16 -0400

    update orb for new boost-scanner-cli (#6)
    * update for boost-new-cli
    * update test job
    * update CHANGELOG and script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,14 @@
 
 All notable changes to this project will be documented in this file.
 
-## Unreleased - [Github](https://github.com/peaudecastor/boost-security-scanner-github/circleci/3.0.0..HEAD)
+## Unreleased - [Github](https://github.com/boostsecurityio/boostsec-scanner-circleci/compare/v4.0.0..HEAD)
 
-## 3.0.0 - 2022-05-26 - [Github](https://github.com/peaudecastor/boost-security-scanner-circleci/releases/tag/3.0.0)
+## v4.0.0 - 2022-10-05 - [Github](https://github.com/boostsecurityio/boostsec-scanner-circleci/compare/v3.0.0...v4.0.0)
+
+- The Boost CLI command and options has changed
+- The Orb command and jobs have changed
+- The Orb now utilizes the Boost Scanner Registry
+
+## v3.0.0 - 2022-05-26 - [Github](https://github.com/boostsecurityio/boostsec-scanner-circleci/releases/tag/v3.0.0)
 
 - Initial Release
-
--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
-set -u
-
-export BOOST_TMP_DIR=${BOOST_TMP_DIR:-${WORKSPACE_TMP:-${TMPDIR:-/tmp}}}
-export BOOST_EXE=${BOOST_EXE:-${BOOST_TMP_DIR}/boost/cli/latest/boost.sh}
-
-
 log.info ()
 { # $@=message
   printf "$(date +'%H:%M:%S') [\033[34m%s\033[0m] %s\n" "INFO" "${*}";
@@ -18,33 +10,41 @@ log.error ()
   printf "$(date +'%H:%M:%S') [\033[31m%s\033[0m] %s\n" "ERROR" "${*}";
 }
 
+git.ls_remote ()
+{
+  git ls-remote --symref origin HEAD \
+    | awk '/^ref:/{sub(/refs\/heads\//, "", $2); print $2}'
+}
+
 init.config ()
 {
   log.info "initializing configuration"
 
+  export BOOST_TMP_DIR=${BOOST_TMP_DIR:-${WORKSPACE_TMP:-${TMPDIR:-/tmp}}}
+  export BOOST_EXE=${BOOST_EXE:-${BOOST_TMP_DIR}/boost-cli/latest}
+
   export BOOST_CLI_URL=${BOOST_CLI_URL:-https://assets.build.boostsecurity.io}
          BOOST_CLI_URL=${BOOST_CLI_URL%*/}
-  export BOOST_DOWNLOAD_URL=${BOOST_DOWNLOAD_URL:-${BOOST_CLI_URL}/boost/get-boost-cli}
-  export BOOST_EXEC_COMMAND=${INPUT_EXEC_COMMAND:-}
+  export BOOST_DOWNLOAD_URL=${BOOST_DOWNLOAD_URL:-${BOOST_CLI_URL}/boost-cli/get-boost-cli}
 
-  export BOOST_MAIN_BRANCH
-         BOOST_MAIN_BRANCH=$(git ls-remote --symref origin HEAD | awk '/^ref:/{sub(/refs\/heads\//, "", $2); print $2}')
-  if [ "${CIRCLE_BRANCH}" != "${BOOST_MAIN_BRANCH}" ]; then
-    export BOOST_GIT_BASE=${BOOST_MAIN_BRANCH}
-  fi
+  export BOOST_GIT_MAIN_BRANCH
+         BOOST_GIT_MAIN_BRANCH=${BOOST_GIT_MAIN_BRANCH:-$(git.ls_remote)}
 
-  if [ -n "${BOOST_API_ENDPOINT_VAR:-}" ]; then
-    export BOOST_API_ENDPOINT=${!BOOST_API_ENDPOINT_VAR}
-  fi
+  init.ci.config
+}
 
-  if [ -n "${BOOST_API_TOKEN_VAR:-}" ]; then
-    export BOOST_API_TOKEN=${!BOOST_API_TOKEN_VAR}
+init.ci.config ()
+{
+  export BOOST_API_TOKEN=${!BOOST_API_TOKEN_VAR}
+
+  if [ "${CIRCLE_BRANCH:-}" != "${BOOST_GIT_MAIN_BRANCH}" ]; then
+    export BOOST_GIT_BASE=${BOOST_GIT_MAIN_BRANCH}
   fi
 }
 
 init.cli ()
 {
-  if [ -f "${BOOST_BIN:-}" ]; then
+  if [ -f "${BOOST_EXE:-}" ]; then
     return
   fi
 
@@ -52,44 +52,19 @@ init.cli ()
   curl --silent "${BOOST_DOWNLOAD_URL}" | bash
 }
 
-main.exec ()
-{
-  init.config
-  init.cli
-
-  if [ -z "${BOOST_EXEC_COMMAND:-}" ]; then
-    log.error "the 'exec_command' option must be defined when in exec mode"
-    exit 1
-  fi
-
-  if [ -z "${BOOST_STEP_NAME:-}" ]; then
-    log.error "the 'step_name' option must be defined in exec mode"
-    exit 1
-  fi
-
-  # shellcheck disable=SC2086
-  exec ${BOOST_EXE} scan exec ${BOOST_CLI_ARGUMENTS:-} --command "${BOOST_EXEC_COMMAND}"
-}
-
 main.scan ()
 {
   init.config
   init.cli
 
-  if [ -n "${BOOST_EXEC_COMMAND:-}" ]; then
-    log.error "the 'exec_command' option must only be defined in exec mode"
-    exit 1
-  fi
-
   # shellcheck disable=SC2086
-  exec ${BOOST_EXE} scan run ${BOOST_CLI_ARGUMENTS:-}
+  exec ${BOOST_EXE} scan repo ${BOOST_CLI_ARGUMENTS:-}
 }
 
-case "${BOOST_ACTION:-scan}" in
-  exec)     main.exec ;;
-  scan)     main.scan ;;
-  *)        log.error "invalid action ${BOOST_ACTION}"
-            exit 1
-            ;;
-esac
+if ! ${BATS_ENABLED:-false}; then
+  set -e
+  set -o pipefail
+  set -u
 
+  main.scan
+fi
